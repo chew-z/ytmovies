7278699c1b0b534e24122e5c28107e511d86cd1b
Robert J. <chew-z@users.noreply.github.com>
2023-09-13 19:15:20 +0200

    Second comming

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 )
 
+type Movie struct {
+	Id     string `form:"id"`
+	Format string `form:"format"`
+	Title  string `form:"title"`
+}
+
 var (
 	ctx, cancel = context.WithCancel(context.Background())
 	path        = os.Getenv("MEDIA_PATH")
@@ -26,19 +32,34 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	router.GET("/", func(c *gin.Context) {
-		if id := c.Query("id"); id == "" {
-			// c.Redirect(http.StatusMovedPermanently, uRL+"media.html")
-			c.HTML(http.StatusOK, "empty.html", gin.H{})
-		} else {
-			format := c.DefaultQuery("f", "")
-			youTubeUrl := "https://www.youtube.com/watch?v=" + id
-			log.Println(youTubeUrl)
+		id := c.DefaultQuery("id", "")
+		format := c.DefaultQuery("f", "")
+		if id != "" {
 			go ytDownload(id, format)
-			u := uRL + id + ".mp4"
-			c.HTML(http.StatusOK, "main.html", gin.H{
-				"URL": u,
-			})
 		}
+		u := uRL + id + ".mp4"
+		c.HTML(http.StatusOK, "main.html", gin.H{
+			"URL": u,
+		})
+
 	})
+	router.POST("/", startPage)
 	router.Run(":" + port)
+}
+
+func startPage(c *gin.Context) {
+	var movie Movie
+	id := ""
+	format := ""
+	if err := c.ShouldBind(&movie); err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	} else {
+		log.Println(movie.Id)
+		id = movie.Id
+		format = movie.Format
+		go ytDownload(id, format)
+	}
+	u := uRL + id + ".mp4"
+	c.Redirect(302, u)
 }
